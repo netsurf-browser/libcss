d91bf786c42bd86a9d9159a6cd8e14e04ee29f39
Michael Drake <michael.drake@codethink.co.uk>
2016-05-08 14:12:19 +0100

    Remove option to support not storing data on DOM nodes.
    It was complicating lifetimes and ownership.

--- test/select.c
+++ test/select.c
@@ -1678,4 +1678,16 @@ static css_error set_libcss_node_data(void *pw, void *n,
 	return CSS_OK;
 }
 
+static css_error get_libcss_node_data(void *pw, void *n,
+		void **libcss_node_data)
+{
+	node *node = n;
+	UNUSED(pw);
+
+	/* Pass any node data back to libcss */
+	*libcss_node_data = node->libcss_node_data;
+
+	return CSS_OK;
+}
+
 
