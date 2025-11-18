07b48859a265b5b088fb8a809b11433b05bc3583
Michael Drake <tlsa@netsurf-browser.org>
2025-11-18 20:06:02 +0000

    select: No need to NULL guard lwc_string_unref

--- src/select/properties/list_style_image.c
+++ src/select/properties/list_style_image.c
@@ -27,8 +27,7 @@ css_error css__set_list_style_image_from_hint(const css_hint *hint,
 
 	error = set_list_style_image(style, hint->status, hint->data.string);
 
-	if (hint->data.string != NULL)
-		lwc_string_unref(hint->data.string);
+	lwc_string_unref(hint->data.string);
 
 	return error;
 }
